75f07e56035bca8e819e538dfc785c8e8a3c4ea0
CaolanCode <cmacmahon3@gmail.com>
2023-04-06 07:00:33 +0100

    commit gameboard test

--- main.bundle.js
+++ main.bundle.js
@@ -16,7 +16,7 @@
   \********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scripts_gameboard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scripts/gameboard */ \"./src/scripts/gameboard.js\");\n\nvar gameboard = (0,_scripts_gameboard__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\nvar ship = gameboard.placeShip(0, 0, 'S', 3);\ngameboard.receiveAttack(0, 2, ship);\nconsole.log(ship);\n\n//# sourceURL=webpack://battleship/./src/app.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scripts_gameboard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scripts/gameboard */ \"./src/scripts/gameboard.js\");\n/* harmony import */ var _scripts_ship__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./scripts/ship */ \"./src/scripts/ship.js\");\n\n\nvar gameboard = (0,_scripts_gameboard__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\nvar ship1 = (0,_scripts_ship__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(2);\nvar ship2 = (0,_scripts_ship__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(4);\ngameboard.placeShip(0, 0, 'S', ship1);\ngameboard.placeShip(0, 0, 'E', ship2);\ngameboard.receiveAttack(0, 2);\nconsole.log(gameboard.ships);\n\n//# sourceURL=webpack://battleship/./src/app.js?");
 
 /***/ }),
 
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scr
   \**********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _ship_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ship.js */ \"./src/scripts/ship.js\");\n\nvar Gameboard = function Gameboard() {\n  var numberOfShips = 0;\n  var gameboard = new Array(10);\n  for (var i = 0; i < 10; i++) {\n    gameboard[i] = new Array(10).fill(false);\n  }\n  var placeShip = function placeShip(i, j, dir, length) {\n    numberOfShips++;\n    var ship = (0,_ship_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(length);\n    if (dir === 'S') {\n      for (var k = 0; k < ship.getLength(); k++) {\n        gameboard[i + k][j] = true;\n      }\n    } else if (dir === 'E') {\n      for (var _k = 0; _k < ship.getLength(); _k++) {\n        gameboard[i][j + _k] = true;\n      }\n    }\n    return ship;\n  };\n  var receiveAttack = function receiveAttack(i, j, ship) {\n    if (gameboard[i][j] === true) {\n      ship.hit();\n      if (ship.isSunk()) numberOfShips--;\n      return true;\n    }\n    return false;\n  };\n  var checkShips = function checkShips() {\n    if (numberOfShips === 0) {\n      return true;\n    }\n    return false;\n  };\n  var getBoard = function getBoard() {\n    return gameboard;\n  };\n  return {\n    placeShip: placeShip,\n    receiveAttack: receiveAttack,\n    checkShips: checkShips,\n    getBoard: getBoard\n  };\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Gameboard);\n\n//# sourceURL=webpack://battleship/./src/scripts/gameboard.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var uuid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! uuid */ \"./node_modules/uuid/dist/esm-browser/v4.js\");\n\nvar Gameboard = function Gameboard() {\n  var ships = [];\n  var gameboard = new Array(10);\n  for (var i = 0; i < 10; i++) {\n    gameboard[i] = new Array(10).fill(false);\n  }\n  var placeShip = function placeShip(i, j, dir, ship) {\n    ship.assignNumber((0,uuid__WEBPACK_IMPORTED_MODULE_0__[\"default\"])());\n    if (dir === 'S') {\n      for (var k = 0; k < ship.getLength(); k++) {\n        gameboard[i + k][j] = ship.getNumber();\n      }\n    } else if (dir === 'E') {\n      for (var _k = 0; _k < ship.getLength(); _k++) {\n        gameboard[i][j + _k] = ship.getNumber();\n      }\n    }\n    ships.push(ship);\n  };\n  var receiveAttack = function receiveAttack(i, j) {\n    if (gameboard[i][j] !== false) {\n      var ship = ships.find(function (item) {\n        return item.getNumber() === gameboard[i][j];\n      });\n      ship.hit();\n      if (ship.isSunk()) ships = ships.filter(function (item) {\n        return item !== ship;\n      });\n      return true;\n    }\n    return false;\n  };\n  var checkShips = function checkShips() {\n    return ships.length === 0 ? true : false;\n  };\n  var getBoard = function getBoard() {\n    return gameboard;\n  };\n  return {\n    placeShip: placeShip,\n    receiveAttack: receiveAttack,\n    checkShips: checkShips,\n    getBoard: getBoard,\n    ships: ships\n  };\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Gameboard);\n\n//# sourceURL=webpack://battleship/./src/scripts/gameboard.js?");
 
 /***/ }),
 
@@ -36,7 +36,67 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar Ship = function Ship(l) {\n  var length = l;\n  var hits = 0;\n  var hit = function hit() {\n    return hits++;\n  };\n  var isSunk = function isSunk() {\n    return length === hits ? true : false;\n  };\n  var getLength = function getLength() {\n    return length;\n  };\n  return {\n    hit: hit,\n    isSunk: isSunk,\n    getLength: getLength\n  };\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Ship);\n\n//# sourceURL=webpack://battleship/./src/scripts/ship.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar Ship = function Ship(l) {\n  var length = l;\n  var hits = 0;\n  var number;\n  var hit = function hit() {\n    return hits++;\n  };\n  var isSunk = function isSunk() {\n    return length === hits ? true : false;\n  };\n  var assignNumber = function assignNumber(n) {\n    return number = n;\n  };\n  var getLength = function getLength() {\n    return length;\n  };\n  var getNumber = function getNumber() {\n    return number;\n  };\n  return {\n    hit: hit,\n    isSunk: isSunk,\n    getLength: getLength,\n    assignNumber: assignNumber,\n    getNumber: getNumber\n  };\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Ship);\n\n//# sourceURL=webpack://battleship/./src/scripts/ship.js?");
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/native.js":
+/*!******************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/native.js ***!
+  \******************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst randomUUID = typeof crypto !== 'undefined' && crypto.randomUUID && crypto.randomUUID.bind(crypto);\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\n  randomUUID\n});\n\n//# sourceURL=webpack://battleship/./node_modules/uuid/dist/esm-browser/native.js?");
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/regex.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/regex.js ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (/^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i);\n\n//# sourceURL=webpack://battleship/./node_modules/uuid/dist/esm-browser/regex.js?");
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/rng.js":
+/*!***************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/rng.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ rng)\n/* harmony export */ });\n// Unique ID creation requires a high quality random # generator. In the browser we therefore\n// require the crypto API and do not support built-in fallback to lower quality random number\n// generators (like Math.random()).\nlet getRandomValues;\nconst rnds8 = new Uint8Array(16);\nfunction rng() {\n  // lazy load so that environments that need to polyfill have a chance to do so\n  if (!getRandomValues) {\n    // getRandomValues needs to be invoked in a context where \"this\" is a Crypto implementation.\n    getRandomValues = typeof crypto !== 'undefined' && crypto.getRandomValues && crypto.getRandomValues.bind(crypto);\n\n    if (!getRandomValues) {\n      throw new Error('crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported');\n    }\n  }\n\n  return getRandomValues(rnds8);\n}\n\n//# sourceURL=webpack://battleship/./node_modules/uuid/dist/esm-browser/rng.js?");
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/stringify.js":
+/*!*********************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/stringify.js ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__),\n/* harmony export */   \"unsafeStringify\": () => (/* binding */ unsafeStringify)\n/* harmony export */ });\n/* harmony import */ var _validate_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./validate.js */ \"./node_modules/uuid/dist/esm-browser/validate.js\");\n\n/**\n * Convert array of 16 byte values to UUID string format of the form:\n * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\n */\n\nconst byteToHex = [];\n\nfor (let i = 0; i < 256; ++i) {\n  byteToHex.push((i + 0x100).toString(16).slice(1));\n}\n\nfunction unsafeStringify(arr, offset = 0) {\n  // Note: Be careful editing this code!  It's been tuned for performance\n  // and works in ways you may not expect. See https://github.com/uuidjs/uuid/pull/434\n  return (byteToHex[arr[offset + 0]] + byteToHex[arr[offset + 1]] + byteToHex[arr[offset + 2]] + byteToHex[arr[offset + 3]] + '-' + byteToHex[arr[offset + 4]] + byteToHex[arr[offset + 5]] + '-' + byteToHex[arr[offset + 6]] + byteToHex[arr[offset + 7]] + '-' + byteToHex[arr[offset + 8]] + byteToHex[arr[offset + 9]] + '-' + byteToHex[arr[offset + 10]] + byteToHex[arr[offset + 11]] + byteToHex[arr[offset + 12]] + byteToHex[arr[offset + 13]] + byteToHex[arr[offset + 14]] + byteToHex[arr[offset + 15]]).toLowerCase();\n}\n\nfunction stringify(arr, offset = 0) {\n  const uuid = unsafeStringify(arr, offset); // Consistency check for valid UUID.  If this throws, it's likely due to one\n  // of the following:\n  // - One or more input array values don't map to a hex octet (leading to\n  // \"undefined\" in the uuid)\n  // - Invalid input values for the RFC `version` or `variant` fields\n\n  if (!(0,_validate_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(uuid)) {\n    throw TypeError('Stringified UUID is invalid');\n  }\n\n  return uuid;\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (stringify);\n\n//# sourceURL=webpack://battleship/./node_modules/uuid/dist/esm-browser/stringify.js?");
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/v4.js":
+/*!**************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/v4.js ***!
+  \**************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _native_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./native.js */ \"./node_modules/uuid/dist/esm-browser/native.js\");\n/* harmony import */ var _rng_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./rng.js */ \"./node_modules/uuid/dist/esm-browser/rng.js\");\n/* harmony import */ var _stringify_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./stringify.js */ \"./node_modules/uuid/dist/esm-browser/stringify.js\");\n\n\n\n\nfunction v4(options, buf, offset) {\n  if (_native_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].randomUUID && !buf && !options) {\n    return _native_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].randomUUID();\n  }\n\n  options = options || {};\n  const rnds = options.random || (options.rng || _rng_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(); // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`\n\n  rnds[6] = rnds[6] & 0x0f | 0x40;\n  rnds[8] = rnds[8] & 0x3f | 0x80; // Copy bytes to buffer, if provided\n\n  if (buf) {\n    offset = offset || 0;\n\n    for (let i = 0; i < 16; ++i) {\n      buf[offset + i] = rnds[i];\n    }\n\n    return buf;\n  }\n\n  return (0,_stringify_js__WEBPACK_IMPORTED_MODULE_2__.unsafeStringify)(rnds);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (v4);\n\n//# sourceURL=webpack://battleship/./node_modules/uuid/dist/esm-browser/v4.js?");
+
+/***/ }),
+
+/***/ "./node_modules/uuid/dist/esm-browser/validate.js":
+/*!********************************************************!*\
+  !*** ./node_modules/uuid/dist/esm-browser/validate.js ***!
+  \********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _regex_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./regex.js */ \"./node_modules/uuid/dist/esm-browser/regex.js\");\n\n\nfunction validate(uuid) {\n  return typeof uuid === 'string' && _regex_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].test(uuid);\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (validate);\n\n//# sourceURL=webpack://battleship/./node_modules/uuid/dist/esm-browser/validate.js?");
 
 /***/ })
 
