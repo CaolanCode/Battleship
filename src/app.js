dda7b42a53c63b483b2ecd5a3350a532e1c33043
CaolanCode <cmacmahon3@gmail.com>
2023-04-06 08:29:24 +0100

    commit header

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
-import Gameboard from "./scripts/gameboard"
-import Ship from "./scripts/ship"
+import {header} from './scripts/dom';
+import './scripts/styles/style.css'
 
-const gameboard = Gameboard()
-const ship1 = Ship(2)
-const ship2 = Ship(4)
-gameboard.placeShip(0,0,'S',ship1)
-gameboard.placeShip(0,0,'E',ship2)
-gameboard.receiveAttack(0,2)
-console.log(gameboard.ships)
+document.body.appendChild(header())
---
+++ src/scripts/dom.js
@@ -0,0 +1,6 @@
+export const header = () => {
+  const container = document.createElement('div')
+  container.classList.add('header')
+  container.innerText = 'Battleship'
+  return container
+}
